ef5c6d39e07df38a925bae85eb09b57387da7046
Zin <vinhtran.vicidev@gmail.com>
2022-09-04 13:42:08 +0700

    add http server

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const http = require('http');
 const app = express();
 
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.send('http server');
+  }
+  const server = http.createServer(requestListener);
+  server.listen(400);
 
 app.get("/", (req, res)=>{
     res.send("hello word")
+})
+app.get("/new", (req, res)=>{
+    res.send("hello new")
 })
 app.listen(process.env.PORT || 5000);
 module.exports = app
