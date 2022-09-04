e673063be10ff32b2ba01e1b7bf4a5f5d19e71da
Zin <vinhtran.vicidev@gmail.com>
2022-09-04 13:46:26 +0700

    remove http server

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require("express");
-const http = require('http');
 const app = express();
-
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.send('http server');
-  }
-  const server = http.createServer(requestListener);
-  server.listen(80);
 
 app.get("/", (req, res)=>{
     res.send("hello word")
