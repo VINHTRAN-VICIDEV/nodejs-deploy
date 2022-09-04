b835b1e88b6dfe6985c5dba38e7416d9a5d62499
Zin <vinhtran.vicidev@gmail.com>
2022-09-04 13:49:01 +0700

    hotfix

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/new", (req, res)=>{
     res.send("hello new")
 })
 app.listen(process.env.PORT || 5000,()=>{
-    console.log("running in :", process.env.POR)
+    console.log("running in :", process.env.PORT)
 });
 module.exports = app
